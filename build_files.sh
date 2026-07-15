b20728bf7a045529d5d57f203fdd118ddb54bdd4
carlos47 <carlosidriss46@gmail.com>
2026-07-16 01:44:47 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "📦 Installation des dépendances..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 
 echo "📁 Collecte des fichiers statiques..."
 python3 manage.py collectstatic --noinput
